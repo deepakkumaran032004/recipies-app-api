5c63af1947737e7de0b3b59ad0d3a9b55b4480fc
deepakkumaran032004 <deepakkumaran032004@gmail.com>
2025-02-17 22:29:35 +0530

    linting done

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,18 +1,26 @@
 import time
+
 from psycopg2 import OperationalError as Psycopg2opError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
+
 class Command(BaseCommand):
-    """ Django command to wait for database """
+    """Django command to wait for database."""
+
     def handle(self, *args, **options):
-        """Entrypoint for command"""
+        """Entrypoint for command."""
         self.stdout.write("Waiting for database...")
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default'])
-                db_up=True
-                self.stdout.write(self.style.SUCCESS('Database  avalibale'))
-            except(Psycopg2opError, OperationalError):
-                self.stdout.write('Database unavailabe, waiting for 1 second...')
+                self.check(databases=["default"])
+                db_up = True
+                self.stdout.write(self.style.SUCCESS(
+                    "Database available"
+                ))
+            except (Psycopg2opError, OperationalError):
+                self.stdout.write(
+                    "Database unavailable, waiting for 1 second..."
+                )
                 time.sleep(1)
